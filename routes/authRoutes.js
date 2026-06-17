d6dbf046ef8eb4bb929232d0a3e360f7b1daa221
Andrey Gonzalez <andrey.gonzalez@mail.mcgill.ca>
2026-06-17 19:38:44 -0400

    Add authentication routes for user registration and login

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('../controllers/authController');
+const { ensureGuest } = require('../middleware/authMiddleware');
+
+router.get('/register', ensureGuest, authController.getRegisterForm);
+router.post('/register', ensureGuest, authController.registerUser);
+
+router.get('/login', ensureGuest, authController.getLoginForm);
+router.post('/login', ensureGuest, authController.loginUser);
+
+router.post('/logout', authController.logoutUser);
+
+module.exports = router;
